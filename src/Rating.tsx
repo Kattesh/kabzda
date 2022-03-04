9967089837e38025de178fc15f3ade4bb940d185
Kate.sh <kate.sh@list.ru>
2022-03-04 18:27:58 +0100

    04.03.2022

---
+++ src/Rating.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+export type RatingValueType = 0 | 1 | 2 | 3 | 4 | 5
+
+type RatingPropsType = {
+    value: RatingValueType
+    onClick:(value:RatingValueType)=> void
+}
+
+export function Rating(props: RatingPropsType) {
+
+    const styles = {
+        starStyle: {
+            color: 'red'
+        }
+    }
+
+    return (
+        <div style={styles.starStyle}>
+            <Star selected={props.value > 0} onClick={props.onClick} value={1}/>
+            <Star selected={props.value > 1} onClick={props.onClick} value={2}/>
+            <Star selected={props.value > 2} onClick={props.onClick} value={3}/>
+            <Star selected={props.value > 3} onClick={props.onClick} value={4}/>
+            <Star selected={props.value > 4} onClick={props.onClick} value={5}/>
+        </div>
+    )
+}
+
+type StarPropsType = {
+    selected: boolean
+    onClick:(value:RatingValueType)=> void
+    value: RatingValueType
+}
+
+function Star(props: StarPropsType) {
+    return <span onClick={()=>(props.onClick(props.value))}>
+    {props.selected ? <>&#9733;&#160;</> : <>&#9734;&#160;</>}
+    </span>
+}
---
+++ src/UncontrolledRating.tsx
@@ -0,0 +1,32 @@
+import React, {useState} from 'react';
+
+export function UncontrolledRating(){
+
+    let[value, setValue]=useState(0)
+
+    const styles={
+        starStyle:{
+            color:'orange'
+        }
+    }
+    return(
+        <div style={styles.starStyle}>
+            <Star selected={value>0} setValue={()=>{setValue(1)}}/>
+            <Star selected={value>1} setValue={()=>{setValue(2)}}/>
+            <Star selected={value>2} setValue={()=>{setValue(3)}}/>
+            <Star selected={value>3} setValue={()=>{setValue(4)}}/>
+            <Star selected={value>4} setValue={()=>{setValue(5)}}/>
+        </div>
+    )
+}
+
+type StarPropsType={
+    selected:boolean
+    setValue: ()=>void
+}
+
+function Star(props:StarPropsType){
+    return <span onClick={()=> {props.setValue()}}>
+    { props.selected ? <>&#9733;&#160;</>:<>&#9734;&#160;</>}
+    </span>
+}
